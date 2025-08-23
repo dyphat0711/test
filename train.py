e0a4841299bd0d556bb20efd8df05fe35bb5a7a6
DuyPhat <duyphathust@gmail.com>
2025-08-23 19:28:37 +0700

    Tranning car ver 1.1

--- train.py
+++ train.py
@@ -1,10 +1,30 @@
-class Solution:
-    def max_subarray_sum(self, arr: list[int]) -> int:
-        if not arr:
-            return []
-            
+class Car:
+    def __init__(self, name, engine):
+        self.name = name
+        self.engine = engine
+        
+    def __str__(self):
+        return f"This is a Car named {self.name} with engine {self.engine}."
+
+class Vehicle(Car):
+    def __init__(self, name, engine):
+        super().__init__(name, engine)
+        self.wheel = 4     
+        
+    def __str__(self):
+        return super().__str__() + f" It is a Vehicle and has {self.wheel} wheels."     
+
+class Moto(Car):
+    def __init__(self, name, engine):
+        super().__init__(name, engine)
+        self.wheel = 2
+        
+    def __str__(self):
+        return super().__str__() + f" It is a Moto and has {self.wheel} wheels."       
             
 if __name__ == "__main__":
-    s = Solution()
+    Yamaha = Moto("R10", "V5")
+    Bmw = Vehicle("XM", "V10")
     
-    print(s.max_subarray_sum([1]))
+    print(Bmw)
+    print(Yamaha)
