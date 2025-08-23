a5372694c1b1ced3903f5efb446b3f93d2a09afb
DuyPhat <duyphathust@gmail.com>
2025-08-23 19:07:07 +0700

    trainning ver 1.0

---
+++ train.py
@@ -0,0 +1,10 @@
+class Solution:
+    def max_subarray_sum(self, arr: list[int]) -> int:
+        if not arr:
+            return []
+            
+            
+if __name__ == "__main__":
+    s = Solution()
+    
+    print(s.max_subarray_sum([1]))
